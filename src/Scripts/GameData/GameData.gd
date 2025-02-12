5de3324521f30b286065035f40c5651b3392e63a
Yni-Viar <81305791+Yni-Viar@users.noreply.github.com>
2025-02-12 14:29:25 +0300

    Optimize package size for Web platform (v0.0.6.2)

--- src/Scripts/GameData/GameData.gd
+++ src/Scripts/GameData/GameData.gd
@@ -4,24 +4,15 @@ extends Resource
 class_name GameData
 
 ## Available inventory items
-@export var items: Array[Item]
+@export var items: Array[Item] = []
 ## Inventory items (use Vector2 as the key and key of item variable as thje value)
 ## E.g. Vector2(1, 1) - 0 means that in position 1, 1 is 0 - Sugar milk
-@export var inventory_items: Dictionary
+@export var inventory_items: Dictionary = {}
 ## Player position
-@export var position: Vector3
-## Storyline. Your choices lead to different ending...
-@export_flags("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15") var story_flag: int = 0
+@export var position: Vector3 = Vector3.ZERO
 ## Health
-@export var health: float
+@export var health: float = 100
 ## Collectable items
-@export var applied_keys: Array[int]
+@export var applied_keys: Array[int] = []
 ## Collected unique items
-@export var collected_item_path: Array[String]
-
-
-# Make sure that every parameter has a default value.
-# Otherwise, there will be problems with creating and editing
-# your resource via the inspector.
-func _init(p_items: Array[Item] = []):
-	items = p_items
+@export var collected_item_path: Array[String] = []
--- src/Scripts/Triggers/ChangeBackgroundTrigger.gd
+++
@@ -1,8 +0,0 @@
-extends BaseTrigger
-
-@export var env: Environment
-
-
-func _on_body_entered(body: Node3D) -> void:
-	if body is PlayerScript:
-		get_tree().root.get_node("Game").change_background(env)
